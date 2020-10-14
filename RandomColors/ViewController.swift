da9f8dcfeb05bdba6a90bee6e086fbb19d19ce0e
luism3861 <eduardo_media@fragua.com.mx>
2020-10-14 13:03:41 -0500

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Luis Eduardo Madina Huerta on 14/10/20.
+//  Copyright © 2020 Luis Eduardo Medina Huerta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
